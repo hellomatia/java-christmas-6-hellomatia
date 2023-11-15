22e7775324471aa660116202713dbe0ce3199afd
hellomatia <shuaigejp80@gmail.com>
2023-11-15 13:49:17 +0900

    feat(domain): 크리스마스 이벤트 정보를 저장하는 기능 추가
    - 이벤트 시작, 종료 날짜와 이름을 저장한다.

---
+++ src/main/java/christmas/domain/event/ChristmasEvents.java
@@ -0,0 +1,45 @@
+package christmas.domain.event;
+
+import java.time.LocalDate;
+
+public enum ChristmasEvents {
+
+    CHRISTMAS_D_DAY_EVENT(LocalDate.of(2023,12,1),
+            LocalDate.of(2023,12,25).plusDays(1),
+            "크리스마스 디데이 할인"),
+    WEEKDAY_EVENT(LocalDate.of(2023,12,1),
+            LocalDate.of(2023,12,31).plusDays(1),
+            "평일 할인"),
+    WEEKEND_EVENT(LocalDate.of(2023,12,1),
+            LocalDate.of(2023,12,31).plusDays(1),
+            "주말 할인"),
+    SPECIAL_EVENT(LocalDate.of(2023,12,1),
+            LocalDate.of(2023,12,31).plusDays(1),
+            "특별 할인"),
+    BONUS_GIFT_EVENT(
+            LocalDate.of(2023,12,1),
+            LocalDate.of(2023,12,31).plusDays(1),
+            "증정 이벤트");
+
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final String name;
+
+    ChristmasEvents(LocalDate startDate, LocalDate endDate, String name) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
